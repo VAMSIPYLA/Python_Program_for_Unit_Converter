05be984b5b5ba61a2ac3d7912ce6c34d5dbc86a7
VAMSIPYLA <vamsiravindra.vr@gmail.com>
2021-03-23 11:31:42 +0530

    updated

--- code.py
+++ code.py
@@ -1,46 +1,48 @@
 # unit converter
 
 num1 = input('Enter the unit value: ')
-unit1 = input('Which unit do you want it converted from:  ')
-unit2 = input('Which unit do you want it converted to: ')
+unit1 = input('Unit you want it convert from:  ')
+unit2 = input('Unit you want it convert to: ')
 
 if unit1 == "cm" and unit2 == "m":
-    ans = float(num1) / 100
-    print(ans)
+    result = float(num1) / 100
+    print("RESULT: ",num1 ,unit1 ,"=",result , unit2)
 elif unit1 == "mm" and unit2 == "cm":
-    ans = float(num1) / 10
-    print(ans)
+    result = float(num1) / 10
+    print("RESULT: ",num1 ,unit1 ,"=",result , unit2)
 elif unit1 == "m" and unit2 == "cm":
-    ans = float(num1) * 100
-    print(ans)
+    result = float(num1) * 100
+    print("RESULT: ",num1 ,unit1 ,"=",result , unit2)
 elif unit1 == "cm" and unit2 == "mm":
-    ans = float(num1) * 10
-    print(ans)
+    result = float(num1) * 10
+    print("RESULT: ",num1 ,unit1 ,"=",result , unit2)
 elif unit1 == "mm" and unit2 == "m":
-    ans = float(num1) / 1000
-    print(ans)
+    result = float(num1) / 1000
+    print("RESULT: ",num1 ,unit1 ,"=",result , unit2)
 elif unit1 == "m" and unit2 == "mm":
-    ans = float(num1) * 1000
-    print(ans)
+    result = float(num1) * 1000
+    print("RESULT: ", num1, unit1, "=", result, unit2)
 elif unit1 == "km" and unit2 == "m":
-    ans = float(num1) * 1000
-    print(ans)
+    result = float(num1) * 1000
+    print("RESULT: ",num1 ,unit1 ,"=",result , unit2)
 elif unit1 == "m" and unit2 == "km":
-    ans = float(num1) / 1000
-    print(ans)
+    result = float(num1) / 1000
+    print("RESULT: ",num1 ,unit1 ,"=",result , unit2)
 elif unit1 == "mm" and unit2 == "km":
-    ans = float(num1) / 1000000
-    print(ans)
+    result = float(num1) / 1000000
+    print("RESULT: ",num1 ,unit1 ,"=",result , unit2)
 elif unit1 == "ft" and unit2 == "cm":
-    ans = float(num1) * 30.48
-    print(ans)
+    result = float(num1) * 30.48
+    print("RESULT: ", num1, unit1, "=", result, unit2)
 elif unit1 == "ft" and unit2 == "mm":
-    ans = float(num1) * 304.8
-    print(ans)
+    result = float(num1) * 304.8
+    print("RESULT: ",num1 ,unit1 ,"=",result , unit2)
 elif unit1 == "ft" and unit2 == "inch":
-    ans = float(num1) * 12
-    print(ans)
+    result = float(num1) * 12
+    print("RESULT: ",num1 ,unit1 ,"=",result , unit2)
 elif unit1 == "inch" and unit2 == "cm":
-    ans = float(num1) * 2.54
+    result = float(num1) * 2.54
+    print("RESULT: ",num1 ,unit1 ,"=",result , unit2)
 elif unit1 == "inch" and unit2 == "mm":
-    ans = float(num1) * 25.4
+    result = float(num1) * 25.4
+    print("RESULT: ",num1 ,unit1 ,"=",result , unit2)
